41d2ee2f1c69e435f0e5da594ad55e74b3ee824a
Jannik Streek <jannik.streek@gmail.com>
2023-01-30 13:17:17 +0100

    finalize new kits design

--- teammapper-frontend/src/app/modules/start/start.component.ts
+++ teammapper-frontend/src/app/modules/start/start.component.ts
@@ -11,6 +11,7 @@ export class StartComponent {
   public projectName: string
   public faGithub = faGithub
   public breakpoint: number
+  public height: number
 
   constructor () {
     this.breakpoint = 1
@@ -18,9 +19,11 @@ export class StartComponent {
 
   ngOnInit() {
     this.breakpoint = (window.innerWidth <= 990) ? 1 : 2;
+    this.height = window.innerHeight
   }
 
   onResize(event: Event) {
     this.breakpoint = ((event.target as Window).innerWidth <= 990) ? 1 : 2;
+    this.height = window.innerHeight
   }
 }
