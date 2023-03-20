2ed4e3385f4600f46994297db4817964ba672c9b
Nicholas <nwittstruck@users.noreply.github.com>
2023-03-20 20:30:02 +0100

    [Feature] use last node modifiedAt instead of map modifiedAt #109: lint

--- teammapper-backend/test/db.ts
+++ teammapper-backend/test/db.ts
@@ -1,5 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 require('dotenv').config();
