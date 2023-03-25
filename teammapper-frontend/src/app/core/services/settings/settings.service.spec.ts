ee1524e6e60d1e6525af306835189767946ac4df
Jannik Streek <jannik.streek@gmail.com>
2023-03-25 11:27:07 +0100

    fix errors

--- teammapper-frontend/src/app/core/services/settings/settings.service.spec.ts
+++ teammapper-frontend/src/app/core/services/settings/settings.service.spec.ts
@@ -1,7 +1,7 @@
-import { CachedAdminMapValue } from "src/app/shared/models/cached-map.model";
-import { HttpService } from "../../http/http.service";
-import { StorageService } from "../storage/storage.service";
-import { SettingsService } from "./settings.service";
+import { CachedAdminMapValue } from 'src/app/shared/models/cached-map.model';
+import { HttpService } from '../../http/http.service';
+import { StorageService } from '../storage/storage.service';
+import { SettingsService } from './settings.service';
 
 describe('SettingsService', () => {
   let settingsService: SettingsService;
@@ -9,42 +9,66 @@ describe('SettingsService', () => {
   let storageServiceSpy: StorageService;
 
   beforeEach(() => {
-    httpServiceSpy = jasmine.createSpyObj('httpService', ['get', 'delete', 'post'])
-    storageServiceSpy = jasmine.createSpyObj('storageService', ['get', 'getAll', 'getAllEntries', 'getAllCreatedMapsFromStorage'])
-  })
+    httpServiceSpy = jasmine.createSpyObj('httpService', [
+      'get',
+      'delete',
+      'post',
+    ]);
+    storageServiceSpy = jasmine.createSpyObj('storageService', [
+      'get',
+      'getAll',
+      'getAllEntries',
+      'getAllCreatedMapsFromStorage',
+    ]);
+  });
 
   it('ignores old maps when returning from storage', async () => {
-    const oldDate = new Date()
+    const oldDate = new Date();
     oldDate.setFullYear(new Date().getFullYear() - 1);
-    const cachedMapDataFromStorage: CachedAdminMapValue = { ttl: oldDate, modificationSecret: '456', adminId: '123', rootName: 'test' };
+    const cachedMapDataFromStorage: CachedAdminMapValue = {
+      ttl: oldDate,
+      modificationSecret: '456',
+      adminId: '123',
+      rootName: 'test',
+    };
     (storageServiceSpy.getAllCreatedMapsFromStorage as any).and.returnValue(
-      new Promise ((resolve) => resolve(
-        [['123', cachedMapDataFromStorage]]
-      ))
+      new Promise(resolve => resolve([['123', cachedMapDataFromStorage]]))
     );
 
     settingsService = new SettingsService(storageServiceSpy, httpServiceSpy);
-    expect(await settingsService.getCachedAdminMapEntries()).toEqual([])
-  })
+    expect(await settingsService.getCachedAdminMapEntries()).toEqual([]);
+  });
 
   it('returns sorted cached maps from storage', async () => {
     const futureDateOne = new Date();
     futureDateOne.setFullYear(new Date().getFullYear() + 2);
     const futureDateTwo = new Date();
     futureDateTwo.setFullYear(new Date().getFullYear() + 1);
-    const cachedMapDataFromStorage: CachedAdminMapValue = { ttl: futureDateOne, modificationSecret: '456', adminId: '123', rootName: 'test' };
-    const otherCachedMapDataFromStorage: CachedAdminMapValue = { ttl: futureDateTwo, modificationSecret: '456', adminId: '123', rootName: 'test' };
+    const cachedMapDataFromStorage: CachedAdminMapValue = {
+      ttl: futureDateOne,
+      modificationSecret: '456',
+      adminId: '123',
+      rootName: 'test',
+    };
+    const otherCachedMapDataFromStorage: CachedAdminMapValue = {
+      ttl: futureDateTwo,
+      modificationSecret: '456',
+      adminId: '123',
+      rootName: 'test',
+    };
     (storageServiceSpy.getAllCreatedMapsFromStorage as any).and.returnValue(
-      new Promise ((resolve) => resolve(
-        [['789', otherCachedMapDataFromStorage], ['123', cachedMapDataFromStorage]]
-      ))
+      new Promise(resolve =>
+        resolve([
+          ['789', otherCachedMapDataFromStorage],
+          ['123', cachedMapDataFromStorage],
+        ])
+      )
     );
 
     settingsService = new SettingsService(storageServiceSpy, httpServiceSpy);
-    expect(await settingsService.getCachedAdminMapEntries()).toEqual(
-      [
-        { id: '123', cachedAdminMapValue: cachedMapDataFromStorage}, {id: '789', cachedAdminMapValue: otherCachedMapDataFromStorage }
-      ]
-    );
+    expect(await settingsService.getCachedAdminMapEntries()).toEqual([
+      { id: '123', cachedAdminMapValue: cachedMapDataFromStorage },
+      { id: '789', cachedAdminMapValue: otherCachedMapDataFromStorage },
+    ]);
   });
 });
