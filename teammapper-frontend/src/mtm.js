694c9b8d284a15a5f6d7ffa35dc1c9f06e37a6bc
Jannik Streek <jannik.streek@gmail.com>
2023-11-15 07:27:00 +0100

    move mtm tracking to file for security policy

---
+++ teammapper-frontend/src/mtm.js
@@ -0,0 +1,6 @@
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+(function() {
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src='https://mtm.teammapper.org/js/container_M235J24H.js'; s.parentNode.insertBefore(g,s);
+})();
