26c9082667cde44de7b3664a0cceca6077158162
Jannik Streek <jannik.streek@gmail.com>
2023-12-05 13:05:56 +0100

    add migration

---
+++ teammapper-backend/src/migrations/1701777634545-AddDetachedPropertyToNodes.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddDetachedPropertyToNodes1701777634545 implements MigrationInterface {
+    name = 'AddDetachedPropertyToNodes1701777634545'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "mmp_node" ADD "detached" boolean NOT NULL DEFAULT false`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "mmp_node" DROP COLUMN "detached"`);
+    }
+
+}
