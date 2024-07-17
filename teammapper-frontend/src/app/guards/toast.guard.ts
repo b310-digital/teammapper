694994ba7833df26a01c06df0982d6b45d816da8
sorenjohanson <soeren@soeren.codes>
2024-07-17 14:57:08 +0000

    changed: replace URL in toast.guard.ts to prevent it from appearing in history

--- teammapper-frontend/src/app/guards/toast.guard.ts
+++ teammapper-frontend/src/app/guards/toast.guard.ts
@@ -29,8 +29,9 @@ export class ToastGuard implements CanActivate {
 
       // This preserves both map UUID and the all important fragment whilst deleting anything toast-related.
       const urlTree = this.router.parseUrl(state.url);
+      console.log(urlTree);
       delete urlTree.queryParams['toastMessage'];
-      this.router.navigateByUrl(urlTree);
+      this.router.navigateByUrl(urlTree, { replaceUrl: true });
 
       return true;
     }
