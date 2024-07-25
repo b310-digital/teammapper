36fe094b1302334ccfe70de1428293d20b7ac046
Jannik Streek <jannik.streek@gmail.com>
2024-07-25 15:35:21 +0200

    prettier run + linted

--- teammapper-frontend/src/app/root.component.ts
+++ teammapper-frontend/src/app/root.component.ts
@@ -42,7 +42,7 @@ export class RootComponent implements OnInit {
     }
 
     // Fix for #347: Force reload of pages in bfcache to prevent broken sync states on macOS where URL and internal state don't match
-    window.addEventListener('pageshow', (event) => {
+    window.addEventListener('pageshow', event => {
       if (event.persisted && window.location.pathname.includes('/map')) {
         window.location.reload();
       }
