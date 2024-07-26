8440d38773b0188374f80a73f611a811673f18b3
Jannik Streek <jannik.streek@gmail.com>
2024-07-26 11:57:46 +0200

    add last maps to start page

--- teammapper-frontend/src/app/modules/start/start.component.ts
+++ teammapper-frontend/src/app/modules/start/start.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { SettingsService } from 'src/app/core/services/settings/settings.service';
+import {
+  CachedAdminMapEntry,
+} from 'src/app/shared/models/cached-map.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'teammapper-start',
@@ -11,14 +16,29 @@ export class StartComponent {
   public faGithub = faGithub;
   public breakpoint: number;
   public height: number;
+  public cachedAdminMapEntries: CachedAdminMapEntry[];
 
-  constructor() {
+  constructor(private settingsService: SettingsService, private router: Router) {
     this.breakpoint = 1;
+    this.cachedAdminMapEntries = [];
   }
-
-  ngOnInit() {
+  
+  async ngOnInit() {
     this.breakpoint = window.innerWidth <= 990 ? 1 : 2;
     this.height = window.innerHeight;
+    this.cachedAdminMapEntries = (await this.settingsService.getCachedAdminMapEntries()).splice(0, 3);
+  }
+
+  public getMapUrl(entry: CachedAdminMapEntry): string {
+    return this.router
+      .createUrlTree([`/map/${entry.id}`], {
+        fragment: entry.cachedAdminMapValue.modificationSecret,
+      })
+      .toString();
+  }
+
+  public getMapTitle(entry: CachedAdminMapEntry): string {
+    return entry.cachedAdminMapValue.rootName || entry.id;
   }
 
   onResize(event: Event) {
