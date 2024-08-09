c14decaa16f13b048fb97f14aac5f83af46be500
sorenjohanson <soeren@soeren.codes>
2024-08-09 13:01:42 +0000

    fix: lint

--- teammapper-frontend/src/app/core/services/mmp/mmp.service.ts
+++ teammapper-frontend/src/app/core/services/mmp/mmp.service.ts
@@ -298,12 +298,7 @@ export class MmpService implements OnDestroy {
     notifyWithEvent?: boolean,
     id?: string
   ) {
-    this.currentMap.instance.updateNode(
-      property,
-      value,
-      notifyWithEvent,
-      id
-    );
+    this.currentMap.instance.updateNode(property, value, notifyWithEvent, id);
   }
 
   /**
