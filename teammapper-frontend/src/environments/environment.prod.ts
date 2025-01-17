682624a9f5c8568644c940f6021613173141b067
JannikStreek <Jannik.streek@gmail.com>
2025-01-17 23:42:40 +0100

    Set pictogram flag to false by default

--- teammapper-frontend/src/environments/environment.prod.ts
+++ teammapper-frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  version: '0.4.0',
+  version: '0.1.9',
   name: 'TeamMapper',
   pictogramApiUrl: '/arasaac/api/pictograms',
   pictogramStaticUrl: '/arasaac/images/pictograms',
-  featureFlagPictograms: true,
+  featureFlagPictograms: false,
 };
