36812f7e0df88a8f7fb6b84e6c7b60e934f375b0
Jannik Streek <jannik.streek@gmail.com>
2025-06-19 20:17:47 +0200

    start draft now working

---
+++ teammapper-frontend/e2e/app.spec.ts
@@ -0,0 +1,38 @@
+import { test } from '@playwright/test';
+
+/*
+* Playwright e2e tests
+*/
+
+test.beforeEach(async ({ page }) => {
+  page.on('console', msg => console.log(msg.text()));
+});
+
+test('creates a document and changes the location', async ({ browser }) => {
+  const context = await browser.newContext({
+    cache: 'disabled',
+    ignoreHTTPSErrors: true,
+    extraHTTPHeaders: {
+      'upgrade-insecure-requests': '0',
+    },
+    args: ['--disable-web-security'],
+  });
+
+  const page = await context.newPage();
+  await page.route('**/*', async route => {
+    console.log('Route intercepted:', route.request().url());
+    console.log(await route.request().allHeaders());
+    route.continue();
+  });
+
+
+
+  page.on('requestfailed', async request => {
+    console.log(await request.allHeaders());
+    console.log('Request failed:', request.url(), request.failure()?.errorText);
+  });
+
+  await page.goto('/');
+
+  await page.waitForSelector('[ng-version]', { timeout: 10000 });
+});
---
+++ teammapper-frontend/playwright.config.ts
@@ -0,0 +1,56 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const connectOptions = process.env.TESTING_PLAYWRIGHT_WS_ENDPOINT
+  ? {
+      connectOptions: {
+        wsEndpoint: process.env.TESTING_PLAYWRIGHT_WS_ENDPOINT ?? '',
+      },
+    }
+  : {};
+
+export default defineConfig({
+  testDir: './e2e',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    ...{
+      /* Base URL to use in actions like `await page.goto('/')`. */
+
+      ignoreHTTPSErrors: true,
+      baseURL:
+        process.env.TESTING_PLAYWRIGHT_BASE_URL ?? 'http://localhost:4200',
+
+      /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+      trace: 'on-first-retry',
+      screenshot: 'only-on-failure',
+    },
+    ...connectOptions,
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+  outputDir: './playwright/output',
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    port: 4200,
+    command: 'npm run --prefix ../teammapper-backend dev',
+    reuseExistingServer: !process.env.CI,
+    stdout: 'pipe',
+    stderr: 'pipe',
+  },
+});
