b4ae5ed4f0b14411e7cd9a680d58218ecd52fbf7
JannikStreek <Jannik.streek@gmail.com>
2025-06-20 18:01:36 +0200

    Add playwright tests (#877)

---
+++ teammapper-frontend/e2e/app.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+/*
+ * Playwright e2e tests
+ */
+
+test('creates a map and changes the location', async ({ page }) => {
+  await page.goto('/');
+  await page.getByText('Create mind map').click();
+  await expect(page.locator('.map').first()).toBeVisible();
+  await expect(page.getByText('Root node')).toBeVisible();
+});
+
+test('adds a new node to the map that is saved and retrieved when reloaded', async ({
+  page,
+}) => {
+  await page.goto('/');
+  await page.getByText('Create mind map').click();
+  await expect(page.getByText('Root node')).toBeVisible();
+  await page.locator("button[title='Adds a node']").first().click();
+  await page.keyboard.type('New Node');
+  // End editing by clicking somewhere else
+  await page.locator('.map').first().click();
+  await expect(page.getByText('New Node')).toBeVisible();
+  await page.reload();
+  // Check if the element is still visible even after reloading, which means it was stored and retrieved from the backend.
+  await expect(page.getByText('New Node')).toBeVisible();
+});
