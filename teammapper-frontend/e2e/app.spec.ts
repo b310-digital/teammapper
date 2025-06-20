35f760823d15043adfd92de5f601a10da8aadd49
Jannik Streek <jannik.streek@gmail.com>
2025-06-20 17:27:11 +0200

    remove proxying completly

--- teammapper-frontend/e2e/app.spec.ts
+++ teammapper-frontend/e2e/app.spec.ts
@@ -4,16 +4,6 @@ import { test, expect } from '@playwright/test';
  * Playwright e2e tests
  */
 
-test.beforeEach(async ({ context }) => {
-//   // Proxy requests to localhost instead of using localhost as its currently used for the dev environment
-//   await context.route(/socket.io|api|arasaac/, route => {
-//     const request = route.request();
-//     const url = new URL(request.url());
-//     url.hostname = 'localhost';
-//     route.continue({ url: url.toString() });
-//   });
-// });
-
 test('creates a map and changes the location', async ({ page }) => {
   await page.goto('/');
   await page.getByText('Create mind map').click();
