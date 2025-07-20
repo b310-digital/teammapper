f95600c52d49691d4bc569525d06bf8fe4f73aad
Jannik Streek <jannik.streek@gmail.com>
2025-07-20 22:31:53 +0200

    fix jest warnings

---
+++ teammapper-frontend/jest.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['node_modules/', 'dist/', 'e2e'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  transform: {
+    '^.+\\.(t|j)s$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/src/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.html$',
+      },
+    ],
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.spec.ts',
+    '!src/test/**/*.ts',
+    '!src/main.ts',
+    '!src/polyfills.ts',
+    '!src/environments/**',
+  ],
+  coverageDirectory: 'coverage',
+  modulePaths: ['<rootDir>'],
+  moduleNameMapper: {
+    '@mmp/index': '<rootDir>/src/test/mocks/mmp-index.ts',
+    '@mmp/map/map': '<rootDir>/src/test/mocks/mmp-map.ts',
+  },
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  maxWorkers: '50%',
+  cacheDirectory: '<rootDir>/.jest-cache',
+};
