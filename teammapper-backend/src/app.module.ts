ddf3330f7f8f5b5835d97ea6999852be260670e3
sguhl <sebastian.guhl@bitglow.de>
2025-09-10 16:24:22 +0200

    Move the serving of the angular app back to app.module.ts (#964)

--- teammapper-backend/src/app.module.ts
+++ teammapper-backend/src/app.module.ts
@@ -3,12 +3,18 @@ import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import configService from './config.service'
 import { MapModule } from './map/map.module'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     MapModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client', 'browser'),
+      exclude: ['/assets/'],
+    }),
   ],
 })
 export default class AppModule {}
