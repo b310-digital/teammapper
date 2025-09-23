e7e5a9a03f87af49ac322dbece1cd8af7921f9b2
JannikStreek <Jannik.streek@gmail.com>
2025-09-23 16:35:14 +0200

    fix tests

--- teammapper-frontend/e2e/node-editing.spec.ts
+++ teammapper-frontend/e2e/node-editing.spec.ts
@@ -67,6 +67,7 @@ test('adds a node and drags it - screenshot test', async ({ page }) => {
   // Take a screenshot of the map after dragging
   await expect(page.locator('.map')).toHaveScreenshot('node-before-drag.png', {
     timeout: 500,
+    maxDiffPixels: 150, 
     animations: 'disabled',
     mask: [page.locator('.mat-toolbar')], // Mask the toolbar as it may vary
   });
@@ -93,6 +94,7 @@ test('adds a node and drags it - screenshot test', async ({ page }) => {
   // Take a screenshot of the map after dragging
   await expect(page.locator('.map')).toHaveScreenshot('node-after-drag.png', {
     timeout: 500,
+    maxDiffPixels: 150, 
     animations: 'disabled',
     mask: [page.locator('.mat-toolbar')], // Mask the toolbar as it may vary
   });
