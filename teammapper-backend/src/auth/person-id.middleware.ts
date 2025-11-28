d93124252ed6e178a981849c1667d9f4030fad18
sguhl <sebastian.guhl@bitglow.de>
2025-11-28 12:22:53 +0100

    Type person id middleware

--- teammapper-backend/src/auth/person-id.middleware.ts
+++ teammapper-backend/src/auth/person-id.middleware.ts
@@ -1,23 +1,31 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import jwt from 'jsonwebtoken';
+import { Injectable, NestMiddleware } from '@nestjs/common'
+import jwt from 'jsonwebtoken'
+import { Request } from '../map/types'
 
 @Injectable()
 export class PersonIdMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
-    const secret = process.env.JWT_SECRET;
-    const cookie = req.cookies?.person_id;
+  use(req: Request, res: Response, next: () => void) {
+    const secret = process.env.JWT_SECRET
+    const cookie = req.cookies?.person_id
 
-    req.pid = undefined;
+    console.warn('Secret and person_id cookie: ' + secret + ' / ' + cookie)
+
+    req.pid = undefined
 
     if (secret && cookie) {
       try {
-        const decoded = jwt.verify(cookie, secret, { algorithms: ['HS256'] }) as { pid: string };
-        req.pid = decoded.pid;
+        const decoded = jwt.verify(cookie, secret, {
+          algorithms: ['HS256'],
+        }) as { pid: string }
+        req.pid = decoded.pid
+        console.log('Decoded person_id: ' + req.pid)
       } catch {
-        console.warn('Invalid person_id cookie');
+        console.warn('Invalid person_id cookie')
       }
+    } else {
+      console.warn('No JWT_SECRET or person_id cookie set')
     }
 
-    next();
+    next()
   }
 }
