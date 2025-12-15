64b94e188bdcce852c7281921e86dcdd01794167
JannikStreek <Jannik.streek@gmail.com>
2025-12-15 08:22:31 +0100

    add owner id migration (#1103)

---
+++ teammapper-backend/src/migrations/1765782220832-AddOwnerId.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddOwnerId1765782220832 implements MigrationInterface {
+    name = 'AddOwnerId1765782220832'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "mmp_map" ADD "ownerExternalId" character varying`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "mmp_map" DROP COLUMN "ownerExternalId"`);
+    }
+
+}
